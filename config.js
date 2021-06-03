3691cf5031be1a819c2f8c52536f7e80b2d23183
Utkarsh Kumar <utk.bkkg@gmail.com>
2021-06-03 17:33:24 +0530

    Basic dashboard

---
+++ config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  "token": "ODQ4NDE1ODAzMTg3MDAzNDAy.YLMSyg.jzMlC5a6djU8M7JB-6GRlExNiTY", // https://discordapp.com/developers/applications/ID/bot
+  "mongodbUrl": "mongodb://localhost/simple-dashboard", // Mongodb connection url.
+  "id": "848415803187003402", // https://discordapp.com/developers/applications/ID/information
+  "clientSecret": "EjAbl6JzBWu6nUfm6H7kVF75YDkPC5ZJ", // https://discordapp.com/developers/applications/ID/information
+  "domain": "http://localhost",
+  "port": 3002,
+  "usingCustomDomain": false
+};
+
+/**
+ * !!!
+ * You need to add a redirect url to https://discordapp.com/developers/applications/ID/oauth2.
+ * Format is: domain:port/callback example http://localhost:8080/callback
+ * - Do not include port if the port is 80.
+ * !!!
+ */
